02b1824e566eef6ae94d63feac054a471b267fe7
unknown <amierasyazlin97@gmail.com>
2019-10-03 01:47:36 +0700

    create static keyword

---
+++ statickeyword.php
@@ -0,0 +1,23 @@
+<?php
+
+class Cars{
+	public $name,
+		   $brand,
+		   $colour,
+		   $maximumSpeed,
+		   $totalPassengers; 
+
+		   public function increaseSpeed() {
+
+		   }
+
+		   public function decreaseSpeed() {
+
+		   }
+
+		   public function replaceTransmission() {
+
+		   }
+}
+
+$avanza = new Cars(); 
